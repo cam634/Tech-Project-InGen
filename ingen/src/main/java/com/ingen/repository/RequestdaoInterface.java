2c6aaa33b0d2071a0fc065896079e49f7793236b
Kstudme <nirlouse630@revature.net>
2022-07-14 17:54:19 -0400

    Nirlouse update create Dao/Interface

---
+++ ingen/src/main/java/com/ingen/repository/RequestdaoInterface.java
@@ -0,0 +1,14 @@
+package com.ingen.repository;
+
+import java.util.List;
+
+import com.ingen.entitiy.Request;
+public interface RequestdaoInterface {
+
+Request createRequest(Request newIdRequest);
+
+List<Request> getallRequest();
+
+
+
+}
