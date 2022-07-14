c2ffcea9b1a7414f58f2ac3d915ba692407b800e
Cameron Hauser <105078054+cam634@users.noreply.github.com>
2022-07-14 10:29:56 -0400

    Updating Hibernate Util

--- ingen/src/main/java/com/ingen/dao/requestdao.java
+++ ingen/src/main/java/com/ingen/dao/requestdao.java
@@ -1,59 +1,23 @@
 package com.ingen.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
+
+import com.ingen.Utils.HibernateUtil;
 import com.ingen.entitiy.Employee;
 
-import org.hibernate.Transaction;
 
 
 public class requestdao {
 
-    private Session session;
-    private Transaction transaction;
-
-
-    private static SessionFactory getsessionFactory(){
-        Configuration cfg = new Configuration();
-
-        cfg.configure("hibernate.cfg.xml");
-
-        SessionFactory factory = cfg.buildSessionFactory();
-
-        return factory;
-    }
-
-
-
-    private void beginTransaction(){
-         this.session = requestdao.getsessionFactory().openSession();
-
-         this.transaction = session.beginTransaction();
-
-    }
-
-    private void endTransaction(){
-        transaction.commit();
-        session.close();
-    }
 
   
-
+   
    public void insert(Employee requestdata){
-    beginTransaction();
-    session.save(requestdata);
-    endTransaction();
+    HibernateUtil.beginTransaction();
+    HibernateUtil.getSession().save(requestdata);
+    HibernateUtil.endTransaction();
    }
 
-
-   public Employee getById(int id) {
-    beginTransaction();
-    Employee requestdata = session.get(Employee.class, id);
-    endTransaction();
-    return requestdata;
-   }
 
     
 }
